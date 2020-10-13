4d58930ffaa6d7daeae240c1a583f1713cadd066
James Brind <jb753@cam.ac.uk>
2020-10-13 17:57:41 +0100

    Revert to datum parameters

--- make_design.py
+++ make_design.py
@@ -4,11 +4,11 @@ Choose parameters for a turbine stage and generate a corresponding CFD model.
 import design
 
 fname = 'input_1.hdf5'
-phi = 0.6  # Flow coefficient
-psi = 2.0  # Stage loading coefficient
-Lam = 0.5  # Degree of reaction
-Ma = 0.8  # Vane exit Mach number
-eta = 0.9  # Polytropic efficiency
+phi = 0.8  # Flow coefficient (0.4 to 1.2)
+psi = 1.6  # Stage loading coefficient (0.8 to 2.4)
+Lam = 0.5  # Degree of reaction (0.4 to 0.6)
+Ma = 0.7  # Vane exit Mach number (0.6 to 0.9)
+eta = 0.9  # Polytropic efficiency (leave this for now)
 gap_chord = 0.5  # Spacing between stator and rotor
 
 design.generate(fname, phi, psi, Lam, Ma, eta, gap_chord )
