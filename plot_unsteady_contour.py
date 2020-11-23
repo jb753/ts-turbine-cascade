feb2a26b0ec8eabb5b950c856fa4b477fe674f9c
James Brind <jb753@cam.ac.uk>
2020-11-23 14:59:26 +0000

    Tweak variable names

--- plot_unsteady_contour.py
+++ plot_unsteady_contour.py
@@ -116,10 +116,10 @@ print(rpms)
 Dat = [probe.secondary(Di, rpmi, cp, ga, P1, T1) for Di, rpmi in zip(Dat,rpms)]
 
 # Determine sector size
-dt_sector = 2. * np.pi * g.get_bv('fracann',0)
+dtheta_sector = 2. * np.pi * g.get_bv('fracann',0)
 
 # Angular movement per time step
-dt_step = Omega * dt
+dtheta_step = Omega * dt
 
 # We must add this as an offset between the blade rows because the mesh is
 # moved to the next time step before the probes are written out!
@@ -132,13 +132,13 @@ for it in range(nt):
     f,a = plt.subplots()  # Create a figure and axis to plot into
     plt.set_cmap('cubehelix_r')
     lev = np.linspace(-8.,35.0,21)
-    probe.render_frame(a, Dat,'ds', it, lev, Omega, dt*nstep_save_probe,nstep_cycle/nstep_save_probe, dt_sector)
+    probe.render_frame(a, Dat,'ds', it, lev, Omega, dt*nstep_save_probe,nstep_cycle/nstep_save_probe, dtheta_sector)
     plt.grid(False)
     a.axis('equal')
     a.axis('off')
-    a.set_ylim([0.,dt_sector*Dat[0]['r'][0,0,0,0]])
+    a.set_ylim([0.,dtheta_sector*Dat[0]['r'][0,0,0,0]])
     plt.tight_layout()  # Remove extraneous white space
-    plt.savefig('%d.png'%it,dpi=200)  
+    plt.savefig('%d.png'%it,dpi=200)
     plt.close(f)
 
 plt.show()  # Render the plot
